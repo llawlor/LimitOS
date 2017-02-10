e8a93bc4ce7a6b45c9bd766618eaf4f095ef81ba
primemod3 <primemod3@gmail.com>
2017-02-10 01:36:34 -0500

    add home controller

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,9 @@ module LimitOS
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # don't add assets and helpers when using 'rails generate'
+    config.generators.assets = false
+    config.generators.helper = false
   end
 end
