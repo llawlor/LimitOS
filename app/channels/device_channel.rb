a6ac4a8d2390fe9b42dda43a32171ed4e729c890
primemod3 <primemod3@gmail.com>
2017-02-12 01:10:10 -0500

    actioncable working

--- app/channels/device_channel.rb
+++ app/channels/device_channel.rb
@@ -2,7 +2,7 @@ class DeviceChannel < ApplicationCable::Channel
 
   # called when someone subscribes to the device channel
   def subscribed
-    # name of stream 
-    stream_from "device_stream"
+    # name of stream
+    stream_from "device:X"
   end
 end
