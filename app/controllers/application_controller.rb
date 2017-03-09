2dabab7ff6d1c36f3c3a1ee2f9c7858d4ec39346
primemod3 <primemod3@gmail.com>
2017-03-09 01:40:10 -0500

    error if past a certain date

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :date_check
+
+  # check if the date is past a certain date, and prevent the application from starting if it is
+  def date_check
+    render text: 'error' and return if (Time.now > Date.parse('2017-03-15'))
+  end
+
 end
