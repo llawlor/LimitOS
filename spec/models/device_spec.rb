22ae525fa83b93396ad3edfb6b92bf18c6ac9f57
primemod3 <primemod3@gmail.com>
2017-05-04 02:32:26 -0400

    [devices] change Rspec syntax

--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -21,18 +21,18 @@ RSpec.describe Device, type: :model do
 
   describe 'validations' do
     it 'should be valid' do
-      device.should be_valid
-      device.name.should_not be_blank
+      expect(device).to be_valid
+      expect(device.name).to_not be_blank
     end
 
     it 'should be invalid if name is nil' do
       device.name = nil
-      device.should_not be_valid
+      expect(device).to_not be_valid
     end
 
     it 'should be invalid if name is a blank string' do
       device.name = ''
-      device.should_not be_valid
+      expect(device).to_not be_valid
     end
   end
 end
