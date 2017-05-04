a76dc5a4fb64fd79cfb96c5c864a69befa11577d
primemod3 <primemod3@gmail.com>
2017-05-04 02:22:56 -0400

    [devices] deprecation fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_filter :date_check
+  before_action :date_check
 
   # check if the date is past a certain date, and prevent the application from starting if it is
   def date_check
