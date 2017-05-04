e168e320465f16bc6f88e58d6c4ebde9d597e1b2
primemod3 <primemod3@gmail.com>
2017-05-04 02:12:16 -0400

    [devices] annotate models

--- spec/models/pin_spec.rb
+++ spec/models/pin_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: pins
+#
+#  id         :integer          not null, primary key
+#  device_id  :integer
+#  name       :string(255)
+#  pin_type   :string(255)
+#  pin_number :integer
+#  min        :integer
+#  max        :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Pin, type: :model do
