f44fb456f1a5ac4dba2cc1732b3ec8b9e53dc656
primemod3 <primemod3@gmail.com>
2017-05-04 01:10:12 -0400

    [devices] fix pins routing

--- spec/routing/pins_routing_spec.rb
+++ spec/routing/pins_routing_spec.rb
@@ -4,35 +4,35 @@ RSpec.describe PinsController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/pins").to route_to("pins#index")
+      expect(:get => "/devices/1/pins").to route_to("pins#index", :device_id => "1")
     end
 
     it "routes to #new" do
-      expect(:get => "/pins/new").to route_to("pins#new")
+      expect(:get => "/devices/1/pins/new").to route_to("pins#new", :device_id => "1")
     end
 
     it "routes to #show" do
-      expect(:get => "/pins/1").to route_to("pins#show", :id => "1")
+      expect(:get => "/devices/1/pins/1").to route_to("pins#show", :id => "1", :device_id => "1")
     end
 
     it "routes to #edit" do
-      expect(:get => "/pins/1/edit").to route_to("pins#edit", :id => "1")
+      expect(:get => "/devices/1/pins/1/edit").to route_to("pins#edit", :id => "1", :device_id => "1")
     end
 
     it "routes to #create" do
-      expect(:post => "/pins").to route_to("pins#create")
+      expect(:post => "/devices/1/pins").to route_to("pins#create", :device_id => "1")
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/pins/1").to route_to("pins#update", :id => "1")
+      expect(:put => "/devices/1/pins/1").to route_to("pins#update", :id => "1", :device_id => "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/pins/1").to route_to("pins#update", :id => "1")
+      expect(:patch => "/devices/1/pins/1").to route_to("pins#update", :id => "1", :device_id => "1")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/pins/1").to route_to("pins#destroy", :id => "1")
+      expect(:delete => "/devices/1/pins/1").to route_to("pins#destroy", :id => "1", :device_id => "1")
     end
 
   end
