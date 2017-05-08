2ac0f222e4abc3b794e1c374f41bfbf9221c189d
primemod3 <primemod3@gmail.com>
2017-05-08 00:18:45 -0400

    [devices] request spec for device management

--- spec/requests/devices_spec.rb
+++ spec/requests/devices_spec.rb
@@ -6,7 +6,55 @@ RSpec.describe "Devices", type: :request do
   let(:user) { FactoryGirl.create(:user) }
   let(:user2) {  FactoryGirl.create(:user) }
   let(:device) { FactoryGirl.create(:device, user: user) }
+  let(:device_attributes) { FactoryGirl.attributes_for(:device) }
 
+  describe 'device management' do
+    it 'creates and views a device' do
+      sign_in user
+
+      # new device
+      get new_device_url
+      expect(response.status).to eq(200)
+
+      # create new device
+      expect {
+        post devices_url, params: { device: device_attributes }
+      }.to change{ user.devices.count }.by(1)
+      new_device = Device.last
+      expect(response).to redirect_to(device_url(new_device))
+
+      # view devices
+      get devices_url
+      expect(response.status).to eq(200)
+      expect(response.body).to include(new_device.name)
+
+      # view the new device
+      get device_url(new_device)
+      expect(response.status).to eq(200)
+      expect(response.body).to include(new_device.name)
+
+      # edit the new device
+      get edit_device_url(new_device)
+      expect(response.status).to eq(200)
+      expect(response.body).to include(new_device.name)
+
+      # update the new device
+      expect {
+        patch device_url(new_device), params: { device: { name: 'my new device name' } }
+      }.to change{ user.devices.count }.by(0)
+      expect(response).to redirect_to(device_url(new_device))
+      follow_redirect!
+      expect(response.body).to include('my new device name')
+
+      # delete the new device
+      expect {
+        delete device_url(new_device)
+      }.to change{ user.devices.count }.by(-1)
+      expect(response).to redirect_to(devices_url)
+      follow_redirect!
+      expect(response.body).to_not include('my new device name')
+    end
+  end
 
   describe '#show' do
     it 'shows a device if it belongs to the user' do
