75e51dd73c45b2021a0f37ade87a373fadca506b
primemod3 <primemod3@gmail.com>
2018-02-21 00:21:16 -0500

    [install-script] update installation comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   # check if the date is past a certain date, and prevent the application from starting if it is
   def date_check
-    render text: 'error' and return if (Time.now > Date.parse('2018-02-24'))
+    render text: 'error' and return if (Time.now > Date.parse('2018-02-28'))
   end
 
   # sets the @devices variable from the user account or the cookies
