dfef7ae8b40f7d04b16ab7a94dc704e41e3b3d08
primemod3 <primemod3@gmail.com>
2018-03-03 00:16:02 -0500

    [save-devices-to-user] add devices from cookies after sign in or sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   # check if the date is past a certain date, and prevent the application from starting if it is
   def date_check
-    render text: 'error' and return if (Time.now > Date.parse('2018-03-08'))
+    render text: 'error' and return if (Time.now > Date.parse('2018-03-10'))
   end
 
   # sets the @devices variable from the user account or the cookies
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,23 @@
 class HomeController < ApplicationController
 
-  def index; ; end
+  # homepage
+  def index
+    # associate devices if necessary
+    associate_devices_with_user if current_user.present? && cookies.encrypted[:device_ids].present?
+  end
+
+  private
+
+    # associate devices with this user
+    def associate_devices_with_user
+      # get the device ids from the cookie
+      device_ids = cookies.encrypted[:device_ids]
+      # get the devices without users that match the ids
+      new_devices = Device.where(user_id: nil).where(id: device_ids)
+      # add the devices
+      new_devices.update_all(user_id: current_user.id)
+      # remove the cookie
+      cookies.delete(:device_ids)
+    end
 
 end
