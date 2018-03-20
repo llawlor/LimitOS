7c2089c5d5b63535bec82524c7ac781e3fbaedb3
primemod3 <primemod3@gmail.com>
2018-03-19 23:32:14 -0400

    [dev] update expiration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   # check if the date is past a certain date, and prevent the application from starting if it is
   def date_check
-    render text: 'error' and return if (Time.now > Date.parse('2018-03-20'))
+    render text: 'error' and return if (Time.now > Date.parse('2018-03-27'))
   end
 
   # sets the @devices variable from the user account or the cookies
