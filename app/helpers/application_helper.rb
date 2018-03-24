869701bd2b8c77121f685673588cdce801b3310b
primemod3 <primemod3@gmail.com>
2018-03-24 00:03:59 -0400

    [lee] display git version of app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,14 @@ module ApplicationHelper
     "http#{ 's' if Rails.env.production?}://#{request.host_with_port}"
   end
 
+  # get the hash of the last git commit
+  def git_version
+    # get the version from git
+    version = `git rev-parse HEAD`.strip
+    # get the version from the REVISION file if the version is longer than 40 characters (due to an error message)
+    version = `cat REVISION`.strip if version.length > 40
+    # return the git version
+    return version
+  end
+
 end
