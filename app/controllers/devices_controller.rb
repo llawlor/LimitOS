234cb857e3b42f31478d0a975606ea9cf86dfd1d
primemod3 <primemod3@gmail.com>
2018-03-29 17:35:54 -0400

    [broadcast-to-device] add form for broadcast to device

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -190,7 +190,7 @@ class DevicesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def device_params
-      params.fetch(:device, {}).permit(:name, :device_type, :i2c_address)
+      params.fetch(:device, {}).permit(:name, :device_type, :i2c_address, :broadcast_to_device_id)
     end
 
     # save this device to the logged out user
