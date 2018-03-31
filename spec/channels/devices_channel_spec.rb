17de6ccb1462307ee1e3444081b5fa2ea733419e
primemod3 <primemod3@gmail.com>
2018-03-31 00:57:44 -0400

    [broadcast-to-device] adding actioncable channel tests

--- spec/channels/devices_channel_spec.rb
+++ spec/channels/devices_channel_spec.rb
@@ -4,25 +4,36 @@ RSpec.describe DevicesChannel, type: :channel do
 
   let(:device) { FactoryBot.create(:device) }
 
-  it 'confirms with correct device id and auth_token' do
-    subscribe(id: device.id, auth_token: device.auth_token)
-    expect(subscription).to be_confirmed
-    expect(streams).to include("devices:#{device.id}")
+  describe '#subscribed' do
+    it 'confirms with correct device id and auth_token' do
+      subscribe(id: device.id, auth_token: device.auth_token)
+      expect(subscription).to be_confirmed
+      expect(streams).to include("devices:#{device.id}")
+    end
+
+    it 'rejects when no device id' do
+      subscribe
+      expect(subscription).to be_rejected
+    end
+
+    it 'rejects when incorrect device id' do
+      subscribe(id: 0)
+      expect(subscription).to be_rejected
+    end
+
+    it 'rejects when incorrect auth token' do
+      subscribe(id: device.id, auth_token: 'INVALID_AUTH_TOKEN')
+      expect(subscription).to be_rejected
+    end
   end
 
-  it 'rejects when no device id' do
-    subscribe
-    expect(subscription).to be_rejected
-  end
-
-  it 'rejects when incorrect device id' do
-    subscribe(id: 0)
-    expect(subscription).to be_rejected
-  end
-
-  it 'rejects when incorrect auth token' do
-    subscribe(id: device.id, auth_token: 'INVALID_AUTH_TOKEN')
-    expect(subscription).to be_rejected
+  describe '#receive' do
+    it 'receives and broadcasts successfully' do
+      subscription = subscribe(id: device.id, auth_token: device.auth_token)
+      broadcasted_message = subscription.receive({ pin: 5, servo: 12 })
+      message_hash = JSON.parse(broadcasted_message.first).symbolize_keys
+      expect(message_hash).to include({ pin: 5, servo: 12 })
+    end
   end
 
 end
