770df8503357cb8d4da091ba8f864c5e925102a9
primemod3 <primemod3@gmail.com>
2018-04-20 00:34:05 -0400

    [auto-update] add git commands to auto-update script

--- config/auto_update.rb
+++ config/auto_update.rb
@@ -7,3 +7,19 @@ remote_version = `git ls-remote https://github.com/llawlor/LimitOS.git refs/head
 puts "##################"
 puts remote_version
 puts GIT_VERSION
+
+# if the remote version is not the same as the local version
+if remote_version != GIT_VERSION
+  # initialize the repository
+  `git init`
+  # add the remote
+  `git remote add origin https://github.com/llawlor/LimitOS.git`
+  # get the branch data
+  `git fetch`
+  # revert local changes
+  `git reset origin/master`
+  # switch to the master branch
+  `git checkout origin/master`
+  # restart the webserver
+  
+end
