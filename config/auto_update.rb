42a1097b8e62ca5403a7062708e87d845fd51227
primemod3 <primemod3@gmail.com>
2018-04-20 00:37:29 -0400

    [auto-update] remove comments

--- config/auto_update.rb
+++ config/auto_update.rb
@@ -4,10 +4,6 @@ require_relative 'initializers/version.rb'
 # get the current version from GitHub master
 remote_version = `git ls-remote https://github.com/llawlor/LimitOS.git refs/heads/master | cut -f 1`
 
-puts "##################"
-puts remote_version
-puts GIT_VERSION
-
 # if the remote version is not the same as the local version
 if remote_version != GIT_VERSION
   # initialize the repository
@@ -21,5 +17,5 @@ if remote_version != GIT_VERSION
   # switch to the master branch
   `git checkout origin/master`
   # restart the webserver
-  
+
 end
