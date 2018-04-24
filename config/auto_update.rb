2c00571f60509640273dcb206ec59df54e13a6c9
primemod3 <primemod3@gmail.com>
2018-04-23 22:17:28 -0400

    [auto-update] restart webservers if necessary

--- config/auto_update.rb
+++ config/auto_update.rb
@@ -4,8 +4,8 @@ require_relative 'initializers/version.rb'
 # get the current version from GitHub master
 remote_version = `git ls-remote https://github.com/llawlor/LimitOS.git refs/heads/master | cut -f 1`
 
-# if the remote version is not the same as the local version
-if remote_version != GIT_VERSION
+# if the git versions are present and the remote version is not the same as the local version
+if !GIT_VERSION.nil? && GIT_VERSION.length > 0 && !remote_version.nil? && remote_version.length > 0 && remote_version != GIT_VERSION
   # initialize the repository
   `git init`
   # add the remote
@@ -16,6 +16,25 @@ if remote_version != GIT_VERSION
   `git reset origin/master`
   # switch to the master branch
   `git checkout origin/master`
-  # restart the webserver
+
+  # check if phusion passenger is running (via nginx)
+  phusion_passenger_is_running = (`ps aux | grep nginx | grep -v grep | wc -l`.to_i > 0)
+  # check if puma is running
+  puma_is_running = (`ps aux | grep puma | grep -v grep | wc -l`.to_i > 0)
+
+  # if phusion passenger is running
+  if phusion_passenger_is_running
+    # restart the webserver
+    `touch tmp/restart.txt`
+  end
+
+  # if puma is running
+  if puma_is_running
+    # get the process ID
+    puma_process_id = `ps aux | grep puma | grep -v grep | grep -v clust | awk '{print $2}'`.to_i
+    # restart the webserver if the process ID is greater than 0
+    `kill -SIGUSR1 #{puma_process_id}` if puma_process_id > 0
+  end
+
 
 end
