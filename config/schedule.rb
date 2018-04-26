4619aaa87b556afba776f88f0b76c856f5ac99fd
primemod3 <primemod3@gmail.com>
2018-04-25 23:47:01 -0400

    [master] don't log crontab output, change auto-update frequency to 5 minutes

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,9 @@
 # set the path
 env :PATH, ENV['PATH']
 # set the output
-set :output, 'log/crontab.log'
+#set :output, 'log/crontab.log'
 
 # run the auto-update rake task on a schedule
-every 1.minute do
+every 5.minutes do
   rake "auto_update:run"
 end
