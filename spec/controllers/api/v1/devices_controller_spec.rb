b90617fbb7d55262ea0199c15b07940f4cdfe20b
primemod3 <primemod3@gmail.com>
2018-05-08 21:51:26 -0400

    [tests] device api tests

--- spec/controllers/api/v1/devices_controller_spec.rb
+++ spec/controllers/api/v1/devices_controller_spec.rb
@@ -6,6 +6,25 @@ RSpec.describe Api::V1::DevicesController, type: :controller do
   let(:device) { FactoryBot.create(:device, user: user) }
   let(:device_without_user) { FactoryBot.create(:device, user: nil) }
 
+  describe '#nodejs_script' do
+    render_views
+
+    it 'returns the script' do
+      post :nodejs_script, params: { id: device.id, auth_token: device.auth_token }
+      expect(response.body).to match('function requestI2CData')
+    end
+
+    it 'returns an error if no device' do
+      post :nodejs_script, params: { id: 0, auth_token: device.auth_token }
+      expect(response.body).to match('Invalid device credentials.')
+    end
+
+    it 'returns an error if invalid auth token' do
+      post :nodejs_script, params: { id: device.id, auth_token: 'INVALID' }
+      expect(response.body).to match('Invalid device credentials.')
+    end
+  end
+
   describe '#create' do
     it 'creates a new device' do
       expect {
