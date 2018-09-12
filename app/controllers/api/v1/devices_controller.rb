126a54e931de319b5ff2cf863c39ffe514e8a365
primemod3 <primemod3@gmail.com>
2018-09-11 21:05:20 -0400

    [pins-api] update pins api

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -19,13 +19,16 @@ class Api::V1::DevicesController < ApplicationController
     message = { "pin" => params[:pin] }
 
     # add a servo message
-    message.merge!({ "servo" => params[:servo] }) if params[:servo].present?
+    message.merge!({ "servo" => params[:servo].to_i }) if params[:servo].present?
 
     # add a digital on/off message
     message.merge!({ "digital" => params[:digital] }) if params[:digital].present?
 
     # broadcast the command to the device
     device.broadcast_message(message)
+
+    # blank http 200 response
+    head :ok
   end
 
   # create a new device
