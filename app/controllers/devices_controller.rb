f49ad5dfff2ef6690b0bb4db0e050ccb3714fa5e
primemod3 <primemod3@gmail.com>
2018-11-27 00:11:46 -0500

    [webcam] add video_invert_vertical to devices form

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -226,7 +226,7 @@ class DevicesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def device_params
-      params.fetch(:device, {}).permit(:name, :device_type, :i2c_address, :broadcast_to_device_id)
+      params.fetch(:device, {}).permit(:name, :device_type, :i2c_address, :broadcast_to_device_id, :video_invert_vertical)
     end
 
     # save this device to the logged out user
