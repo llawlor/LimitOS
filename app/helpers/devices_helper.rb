fe393a7fcb744158d388aa2eb06ebc19cd1fcc3d
primemod3 <primemod3@gmail.com>
2018-11-29 23:19:15 -0500

    [webcam] add helpers for video websocket urls

--- app/helpers/devices_helper.rb
+++ app/helpers/devices_helper.rb
@@ -1,5 +1,15 @@
 module DevicesHelper
 
+  # host with port for video going to clients
+  def video_to_clients_url
+    return Rails.env.production? ? 'wss://limitos.com' : 'ws://192.168.1.101:8082'
+  end
+
+  # host with port for video coming from devices
+  def video_from_devices_url
+    return Rails.env.production? ? 'wss://limitos.com' : 'ws://192.168.1.101:8081'
+  end
+
   # url for the websocket server
   def websocket_server_url
     return Rails.env.production? ? 'wss://limitos.com/cable' : "ws://#{request.host}:#{request.port}/cable"
