264cc34e3f3e24dc0fea09191822ee1d65d32db3
primemod3 <primemod3@gmail.com>
2018-12-08 00:09:35 -0500

    [webcam] refactor device video_from_devices_url

--- app/helpers/devices_helper.rb
+++ app/helpers/devices_helper.rb
@@ -1,10 +1,5 @@
 module DevicesHelper
 
-  # host with port for video coming from devices
-  def video_from_devices_url
-    return Rails.env.production? ? 'wss://limitos.com' : 'ws://192.168.1.101:8081'
-  end
-
   # url for the websocket server
   def websocket_server_url
     return Rails.env.production? ? 'wss://limitos.com/cable' : "ws://#{request.host}:#{request.port}/cable"
