b0d36a64d4aa5026efc68379ff3eb2423df328c5
primemod3 <primemod3@gmail.com>
2018-12-21 00:21:13 -0500

    [auto-update-node-script] move method

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -21,18 +21,6 @@ class Registration < ApplicationRecord
   # remove leading and trailing whitespaces
   strip_attributes
 
-  # get last update date of node.js install script
-  def self.install_script_updated_at
-    # get the git log results
-    git_output = `git log -n1 app/views/shared/_nodejs_script.html.erb`
-    # get the date portion of the results
-    date_line = git_output.split("\n")[2]
-    # get the date string
-    date_string = date_line.split("Date:")[1].strip
-    # output the parsed time
-    return Time.parse(date_string)
-  end
-
   # generate a secure authentication token
   def set_auth_token
     # generate the 6-character auth token
