8cbeb07e376039faba9644a0dace3df5152aaca2
primemod3 <primemod3@gmail.com>
2018-12-31 15:42:56 -0500

    [auto-update-node-script] move reinstall nodejs script command to a helper

--- app/helpers/devices_helper.rb
+++ app/helpers/devices_helper.rb
@@ -5,4 +5,9 @@ module DevicesHelper
     return Rails.env.production? ? 'wss://limitos.com/cable' : "ws://#{request.host}:#{request.port}/cable"
   end
 
+  # linux shell command to reinstall the nodejs script
+  def reinstall_script_command(device)
+    return "curl -sS --data \"auth_token=#{ device.master_device.auth_token }\" #{ full_url }/devices/#{ device.master_device.id }/install | bash"
+  end
+
 end
