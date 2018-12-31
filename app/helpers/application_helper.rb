3ff82fe15e385ada8d483b97424a0f28556b8ede
primemod3 <primemod3@gmail.com>
2018-12-31 17:05:22 -0500

    [lee] update method name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   # full url of the server, for example: https://limitos.com or http://localhost:3000
-  def full_url
+  def full_server_url
     "http#{ 's' if Rails.env.production?}://#{request.host_with_port}"
   end
 
