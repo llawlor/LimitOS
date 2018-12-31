69b9de0232a32fc3c553d92c1d4283b942f5d5bc
primemod3 <primemod3@gmail.com>
2018-12-31 16:33:24 -0500

    [master] moving websocket server to config file

--- app/helpers/devices_helper.rb
+++ app/helpers/devices_helper.rb
@@ -1,10 +1,5 @@
 module DevicesHelper
 
-  # url for the websocket server
-  def websocket_server_url
-    return Rails.env.production? ? 'wss://limitos.com/cable' : "ws://#{request.host}:#{request.port}/cable"
-  end
-
   # linux shell command to reinstall the nodejs script
   def reinstall_script_command(device)
     return "curl -sS --data \"auth_token=#{ device.master_device.auth_token }\" #{ full_url }/devices/#{ device.master_device.id }/install | bash"
