b710c193a70a44d45bb2649efa8f6ebf4790488c
primemod3 <primemod3@gmail.com>
2019-02-08 00:02:26 -0500

    [drive-controls] annotate

--- spec/models/synchronized_pin_spec.rb
+++ spec/models/synchronized_pin_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: synchronized_pins
+#
+#  id                 :integer          not null, primary key
+#  pin_id             :integer
+#  synchronization_id :integer
+#  device_id          :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  value              :string(255)
+#
+
 require 'rails_helper'
 
 RSpec.describe SynchronizedPin, type: :model do
