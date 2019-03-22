a774e375fcb58e0d7ff1034c762bff98be359f0f
primemod3 <primemod3@gmail.com>
2019-03-22 00:22:46 -0400

    [lee] add helper for image meta tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,12 @@ module ApplicationHelper
 
   # full url of the server, for example: https://limitos.com or http://localhost:3000
   def full_server_url
-    "http#{ 's' if Rails.env.production?}://#{request.host_with_port}"
+    return "http#{ 's' if Rails.env.production?}://#{request.host_with_port}"
+  end
+
+  # meta tag for images, so that thumbnails appear correctly on social media
+  def image_meta_tag(image_path)
+    return "<meta property='og:image' content='#{ asset_path(image_path) }' />".html_safe
   end
 
 end
