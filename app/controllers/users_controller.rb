d467222005aa1071e3c1baf76aff9cc81580c422
primemod3 <primemod3@gmail.com>
2019-11-12 22:53:33 -0500

    [lee] redirect from account page if no user logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
 
   def account
+    # get the user
     @user = current_user
+    # redirect if no user
+    redirect_to root_path and return if @user.blank?
   end
 
 end
