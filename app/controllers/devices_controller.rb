a458962a0b76a264a0ecba9a30d42ffb505996b9
primemod3 <primemod3@gmail.com>
2020-12-30 00:01:03 -0500

    [audio] add audio fields

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -254,7 +254,7 @@ class DevicesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def device_params
-      params.fetch(:device, {}).permit(:name, :device_type, :i2c_address, :broadcast_to_device_id, :video_enabled, :invert_video, :public_video, :slug)
+      params.fetch(:device, {}).permit(:name, :device_type, :i2c_address, :broadcast_to_device_id, :video_enabled, :invert_video, :public_video, :slug, :audio_enabled, :audio_start_pin)
     end
 
     # save this device to the logged out user
